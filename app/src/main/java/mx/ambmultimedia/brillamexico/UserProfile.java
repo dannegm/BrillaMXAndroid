3fe7552a313ec2ed1c51de3d20a3a69c472cfc08
dannegm <danneg.m@gmail.com>
2015-03-02 20:20:43 -0600

    Navigation drawer, Login Steps & User Profile

--- app/src/main/java/mx/ambmultimedia/brillamexico/UserProfile.java
+++ app/src/main/java/mx/ambmultimedia/brillamexico/UserProfile.java
@@ -1,35 +1,24 @@
 package mx.ambmultimedia.brillamexico;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.Toolbar;
-import android.support.v4.widget.DrawerLayout;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class UserProfile extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-
-        NavDrawerFrag navDrawerFragment = (NavDrawerFrag) getSupportFragmentManager().findFragmentById(R.id.navDrawer);
-        DrawerLayout drawer_layout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        navDrawerFragment.setUp(R.id.navDrawer, drawer_layout, toolbar);
+        setContentView(R.layout.activity_user_profile);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_user_profile, menu);
         return true;
     }
 
